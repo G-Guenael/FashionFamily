d1d9657555bb43fddbd5384ef19598c87223b68e
G-Guenael <g.guenael@gmail.com>
2026-03-24 18:16:18 +0100

    Ajout test.php

---
+++ test.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<p> Lorem ipsum dolor sit amet consectetur, adipisicing elit. Necessitatibus asperiores eos, enim animi veniam consequuntur aliquam esse similique consectetur repellendus! Quia asperiores a dolor recusandae rerum reprehenderit quas possimus consequuntur fugiat voluptatum? Libero, temporibus commodi ex fuga amet ab sapiente dicta alias, inventore quo assumenda sunt quisquam nulla quas velit officia, doloribus quos ea eius possimus autem enim officiis? Asperiores, corporis. Sequi quos amet, aliquid tempore iste delectus ex quaerat earum ipsum, distinctio, voluptatibus nam nostrum eos minus nulla possimus incidunt nihil saepe modi quam dignissimos? Laudantium consequatur nisi perspiciatis debitis totam vero labore sapiente officia doloremque, facilis hic porro! </p>
+
+
+</body>
+</html>
