dc71b8f390f56ca83c40f59acd0d69512ad59929
G-Guenael <g.guenael@gmail.com>
2026-03-30 16:35:25 +0200

    update page Dashboard

--- dashboard/admin.js
+++ dashboard/admin.js
@@ -1,23 +1,58 @@
+// console.log("JS chargé !");
+
 const content = document.getElementById("content");
 
-async function loadPage(page){
-    try {
-        content.innerHTML = "<p>Chargement...</p>";
+async function loadPage(page) {
+  try {
+    content.innerHTML = "<p>Chargement...</p>";
 
-        const response = await fetch(`pages/${page}.html`);
-        const data = await response.text();
+    const response = await fetch(`pages/${page}.html`);
+    const data = await response.text();
 
-        content.innerHTML = data;
-    } catch (error) {
-        content.innerHTML = "<h1>Erreur de chargement</h1>"
+    content.innerHTML = data;
+    if (page === "dashboard") {
+      initDashboard();
     }
+  } catch (error) {
+    content.innerHTML = "<h1>Erreur de chargement</h1>";
+  }
 }
 
-document.querySelectorAll(".sidebar a").forEach(link => {
-    link.addEventListener("click", ()=> {
-        const page = link.dataset.page
-        loadPage(page)
-    })
-})
+document.querySelectorAll(".sidebar a").forEach((link) => {
+  link.addEventListener("click", () => {
+    const page = link.dataset.page;
+    loadPage(page);
+  });
+});
 
-loadPage("dashboard")
+loadPage("dashboard");
+
+// --------------------------Graphique--------------------------
+
+// Injecter valeurs
+function initDashboard() {
+  const data = {
+    sales: 4.235,
+    orders: 734,
+    revenue: 12450,
+    customers: 180,    
+  };
+
+  const salesEl = document.getElementById("sales");
+
+  if (salesEl) {
+    salesEl.textContent = `${data.sales} €`;
+  };
+
+  const customersEl = document.getElementById("customers");
+
+  if (customersEl) {
+    customersEl.textContent = `${data.customers}`;
+  };
+
+  const ordersEl = document.getElementById("orders");
+
+  if (ordersEl) {
+    ordersEl.textContent = `${data.orders}`;
+  }
+}
