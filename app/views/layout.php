b41dae61223cb066c3bab4fe6ebbe80ef645740c
Kevin <k.urbain.personnel@gmail.com>
2026-04-01 18:45:31 +0200

    /

--- app/views/layout.php
+++ app/views/layout.php
@@ -14,7 +14,8 @@
 </head>
 
 <body>
-    <div id="header" data-logged-in="<?= Auth::isLoggedIn() ? '1' : '0' ?>" data-base-url="<?= BASE_URL ?>" data-cart-count="<?= Cart::getCount() ?>"></div>
+    <div id="header" data-logged-in="<?= Auth::isLoggedIn() ? '1' : '0' ?>" data-base-url="<?= BASE_URL ?>"
+        data-cart-count="<?= Cart::getCount() ?>"></div>
 
     <?= $content ?>
 
